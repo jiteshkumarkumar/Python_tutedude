76a07ab4b74664fbcf106dac6dce5959329b686b
Jitesh Kumar <jitesh.kumar142.jk@gmail.com>
2025-11-25 02:34:23 +0530

    Add files via upload

---
+++ ASSIGNMENT 1.py
@@ -0,0 +1,13 @@
+num1 =int(input("Enter first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition : ", num1 + num2)
+print("Subtraction : ", num1 - num2)
+print("Multipication : ", num1 * num2)
+print("Division : ", num1 / num2)
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Hello {first_name} {last_name}! Welcome to python programming!")
+
+
